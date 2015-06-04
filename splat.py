741f294e4005ca155c6445924873859ccc6c5f66
James Judd <juddling@assassinators.net>
2015-06-04 18:20:19 +0100

    delete bouncing ball

--- splat.py
+++ splat.py
@@ -4,15 +4,11 @@ from Bug import Bug
 pygame.init()
 
 size = width, height = 600, 600
-speed = [2, 2]
 black = 0, 0, 0
 FPS = 30
 sprites = pygame.sprite.Group()
 
 screen = pygame.display.set_mode(size)
-
-ball = pygame.image.load("ball.gif")
-ballrect = ball.get_rect()
 
 margin = 100
 
@@ -30,16 +26,7 @@ while 1:
 		if event.type == pygame.QUIT:
 			sys.exit()
 
-	ballrect = ballrect.move(speed)
-
-	if ballrect.left < 0 or ballrect.right > width:
-		speed[0] = -speed[0]
-
-	if ballrect.top < 0 or ballrect.bottom > height:
-		speed[1] = -speed[1]
-
 	screen.fill(black)
-	screen.blit(ball, ballrect)
 
 	# calls update method on all sprites in group
 	sprites.update()
