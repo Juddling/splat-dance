d7404ad550a233ced0c74f569b09ab9d0902e1e1
James Judd <juddling@assassinators.net>
2015-06-04 21:34:39 +0100

    bugs know their positions, play sound on squish

--- Bug.py
+++ Bug.py
@@ -1,10 +1,11 @@
 import pygame
-import splat
 
 
 class Bug(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, position):
         pygame.sprite.Sprite.__init__(self)
+
+        self.position = position
 
         self.image = pygame.image.load("bug.png")
         self.size = self.image.get_size()
@@ -13,10 +14,28 @@ class Bug(pygame.sprite.Sprite):
         self.rect = (x - self.size[0]/2, y - self.size[1]/2)
 
         # after this number of frames, the bug will disappear
-        self.lifespan = 5
+        self.lifespan = 90
 
     def update(self):
         self.lifespan = self.lifespan - 1
 
         if self.lifespan <= 0:
             self.kill()
+
+    def squish(self):
+        self.kill()
+
+    def is_bug_at_position(self, key):
+        if key == pygame.K_UP and self.position == 1:
+            return True
+
+        if key == pygame.K_DOWN and self.position == 2:
+            return True
+
+        if key == pygame.K_LEFT and self.position == 3:
+            return True
+
+        if key == pygame.K_RIGHT and self.position == 4:
+            return True
+
+        return False
--- splat.py
+++ splat.py
@@ -13,6 +13,19 @@ sprites = pygame.sprite.Group()
 screen = pygame.display.set_mode(size)
 
 
+def load_sound(name):
+    class NoneSound:
+        def play(self): pass
+    if not pygame.mixer or not pygame.mixer.get_init():
+        return NoneSound()
+    try:
+        sound = pygame.mixer.Sound(name)
+    except pygame.error:
+        print ('Cannot load sound: %s' % fullname)
+        raise SystemExit(str(geterror()))
+    return sound
+
+
 def random_bug():
     """
     create a random bug in one of the four positions:
@@ -22,20 +35,26 @@ def random_bug():
     margin = 100
 
     if x == 1:
-        return Bug(width/2, margin)
+        return Bug(width/2, margin, 1)
     elif x == 2:
-        return Bug(width/2, height-margin)
+        return Bug(width/2, height-margin, 2)
     elif x == 3:
-        return Bug(margin, height/2)
+        return Bug(margin, height/2, 3)
 
-    return Bug(width-margin, height/2)
+    return Bug(width-margin, height/2, 4)
 
 fps_clock = pygame.time.Clock()
+punch_sound = load_sound("punch.wav")
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            for bug in sprites.sprites():
+                if bug.is_bug_at_position(event.key):
+                    punch_sound.play()
+                    bug.squish()
 
     screen.fill(black)
 
